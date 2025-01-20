1b59f191a2906f321cba6e1233528f3a13a492f9
HeHHeyboi <thanathatwave@gmail.com>
2025-01-20 21:24:04 +0700

    Test go with database, Pass

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	_ "modernc.org/sqlite"
+	"os"
+)
+
 func main() {
-	print("Hello world2")
+	dbName := "main.db"
+	if len(os.Args) > 1 && os.Args[1] == "test" {
+		dbName = "test.db"
+	}
+
+	db, err := sql.Open("sqlite", dbName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	fmt.Println("success")
 }
