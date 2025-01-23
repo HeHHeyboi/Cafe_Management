64f82b3b72297ee87d67efd4efb812eda68f9a4e
HeHHeyboi <thanathatwave@gmail.com>
2025-01-23 16:53:04 +0700

    When run program will setup db for testing
    Add: query for setup database in const.go
    Change: gallery.go improve error message
    Change: Driver for db

--- backend/sql/schema/002_gallery.sql
+++ backend/sql/schema/002_gallery.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS gallery(
 	StartDate TEXT NOT NULL,
 	EndDate TEXT NOT NULL,
 	DESC TEXT,
-	user_id UUID
+	user_id UUID,
+	FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
 -- +goose Down
