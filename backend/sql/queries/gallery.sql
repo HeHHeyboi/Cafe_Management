abfbe6c548cec549e1a1be20a0a9c0da37cb0fca
HeHHeyboi <thanathatwave@gmail.com>
2025-01-27 12:21:44 +0700

    Add: Query for list gallery in this month
    Add: url Query param for GET /gallery API

--- backend/sql/queries/gallery.sql
+++ backend/sql/queries/gallery.sql
@@ -1,5 +1,16 @@
 -- name: ListGallery :many
-select * from gallery;
+select * from gallery
+ORDER BY "StartDate";
+
+-- FIX: This Query next time, May be use WITH-CLAUSE to handle variable
+-- name: ListGalleryByMonth :many
+SELECT * FROM gallery
+WHERE StartDate >= CASE 
+	WHEN (CAST(@this_month AS INT )) THEN date('now','start of month') ELSE '0' 
+END
+AND StartDate < CASE 
+	WHEN (CAST(@this_month AS INT) ) THEN date('now','start of month','+1 month') ELSE '99999' 
+END;
 
 -- name: BookGallery :one
 INSERT INTO gallery(Gname,StartDate,EndDate,Desc,user_id) VALUES (?,?,?,?,?)
