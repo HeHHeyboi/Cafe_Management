3b6d1a32ec1328ff9eb43e7bf2c2523ed0ed6bda
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-03-01 22:19:40 +0700

    Update Homepage

---
+++ nextjs/src/app/components/Navbar/Logo.js
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className="flex items-center ">
+      <a href="/">
+        <img src="/Logo.png" alt="Logo" className="h-30 w-30"/>
+      </a>
+    </div>
+  );
+}
+
+export default Logo;
