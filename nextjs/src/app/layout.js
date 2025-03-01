36fa510c7ef310035bfa571b38e8692006f3db5e
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-03-01 19:31:59 +0700

    Revert "update"
    This reverts commit 0126ce43ea7074c369459d5e84745c87d7fec59d.

--- nextjs/src/app/layout.js
+++ nextjs/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../../components/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
