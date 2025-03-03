f2d4d5d90b5201c850590e4df856dd210fac034e
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-03-04 00:54:35 +0700

    Fix Seep And ColorMenuBar

--- nextjs/src/app/components/MenuBar/page.js
+++ nextjs/src/app/components/MenuBar/page.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { MenuSquare, Image, Gift } from "lucide-react";
+import { Coffee } from 'lucide-react';
 
 const MenuItem = ({ Icon, label, href }) => {
     return (
         <Link href={href} className="flex flex-col items-center gap-2">
-            <div className="w-16 h-16 flex items-center justify-center bg-gray-200 rounded-lg transition hover:bg-gray-300">
-                <Icon className="w-8 h-8 text-gray-800" />
+            <div className="w-16 h-16 flex items-center justify-center bg-[#233f87] rounded-lg transition hover:bg-[#1e3573]">
+                <Icon className="w-8 h-8 text-white" />
             </div>
             <p className="text-sm font-semibold text-gray-800">{label}</p>
         </Link>
@@ -14,13 +15,13 @@ const MenuItem = ({ Icon, label, href }) => {
 
 const Menu = () => {
     const items = [
-        { Icon: MenuSquare, label: "Menu", href: "/menu" },
+        { Icon: Coffee, label: "Menu", href: "/menu" },
         { Icon: Image, label: "Gallery", href: "/gallery" },
         { Icon: Gift, label: "GiveAway", href: "/GiveAwayShow" },
     ];
 
     return (
-        <div className="flex flex-wrap justify-center gap-8 p-6 bg-white rounded-lg shadow-md">
+        <div className="flex flex-wrap justify-center gap-8 p-4 bg-white shadow-lg rounded-lg">
             {items.map((item, index) => (
                 <MenuItem key={index} {...item} />
             ))}
