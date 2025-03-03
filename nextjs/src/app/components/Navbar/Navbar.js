214a5496a3349ae763b7976751348056a46d9691
DuckVinCiTH <151752977+PedGrob@users.noreply.github.com>
2025-03-03 17:52:20 +0700

    a bit change

--- nextjs/src/app/components/Navbar/Navbar.js
+++ nextjs/src/app/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import Dropdownlist from './dropdownlist'
 
 const Navbar = () => {
   return (
-    <nav className="container flex justify-between text-lg"> 
+    <nav className="container flex justify-between py-4 px-4 sm:px-8"> 
       <Logo />
 
       <div className="flex gap-3 items-center">
