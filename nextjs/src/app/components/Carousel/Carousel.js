889a9dd2d3bbc764a5c2d76d6a2b22d052e46e9d
DuckVinCiTH <151752977+PedGrob@users.noreply.github.com>
2025-03-03 18:16:17 +0700

    fix error

--- nextjs/src/app/components/Carousel/Carousel.js
+++ nextjs/src/app/components/Carousel/Carousel.js
@@ -11,11 +11,7 @@ const Carousel = () => {
   const settings = {
     infinite: true,
     speed: 500,
-<<<<<<< Updated upstream
-    slidesToShow: 1,
-=======
     slidesToShow: 4,
->>>>>>> Stashed changes
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -30,7 +26,6 @@ const Carousel = () => {
   ];
 
   return (
-<<<<<<< Updated upstream
     <div className="carousel-container">
       <Slider {...settings}>
         {images.map((img, index) => (
@@ -49,13 +44,6 @@ const Carousel = () => {
         ))}
       </Slider>
     </div>
-=======
-    <div style={{ width: "80%", margin: "auto" }}>
-    <Slider {...settings}>
-    <div><img src="/Test1.jpg" alt="test1"/></div>
-  </Slider>
-  </div>
->>>>>>> Stashed changes
   );
 };
 
