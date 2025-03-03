7e1a9b5ca4486aa273f4278bc025ca1eb7e55e88
HeHHeyboi <thanathatwave@gmail.com>
2025-03-03 17:33:10 +0700

    Revert "Merge branch 'main' of https://github.com/HeHHeyboi/Cafe_Management"
    This reverts commit 923265e6b0eb3174c080acdca6998cd7763071a0, reversing
    changes made to 791edc2cba93a972a0f6dfc9e9db72b77782f2a6.

--- nextjs/src/app/components/Carousel/Carousel.js
+++ nextjs/src/app/components/Carousel/Carousel.js
@@ -2,48 +2,26 @@
 
 import React from "react";
 import Slider from "react-slick";
-import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./Carousel.css";
 
 const Carousel = () => {
   const settings = {
+    dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
   };
 
-  const images = [
-    { src: "/Note.jpg", alt: "Test 1" },
-    { src: "/Jaemin.jpg", alt: "Test 2" },
-    { src: "/Note.jpg", alt: "Test 3" },
-    { src: "/Note.jpg", alt: "Test 4" },
-    { src: "/Note.jpg", alt: "Test 5" },
-  ];
-
   return (
-    <div className="carousel-container">
-      <Slider {...settings}>
-        {images.map((img, index) => (
-          <div key={index} className="carousel-slide">
-            <div className="relative w-full h-[31.25vw] max-h-[500px]">
-              <Image 
-                src={img.src} 
-                alt={img.alt} 
-                fill 
-                className="object-cover "
-                priority={index === 0} 
-                sizes="100vw"
-              />
-            </div>
-          </div>
-        ))}
-      </Slider>
-    </div>
+    <div style={{ width: "80%", margin: "auto" }}>
+    <Slider {...settings}>
+    <div><img src="/Test1.jpg" alt="test1"/></div>
+  </Slider>
+  </div>
   );
 };
 
--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -2,13 +2,11 @@
 import dropdownlist from "./components/Navbar/dropdownlist";
 import React from "react";
 import Carousel from "./components/Carousel/Carousel";
-import Menu from "./components/MenuBar/page";
 export default function Home() {
   return (
     <div >
       <dropdownlist />
       <Carousel />
-      <Menu />
         <h1>Home</h1>
     </div>
   );
