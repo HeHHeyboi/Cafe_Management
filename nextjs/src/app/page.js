b41391b1d784fd36cb2341a97eb547919e857a2a
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-03-03 14:10:59 +0700

    Update Slide

--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -1,9 +1,12 @@
-import dropdownlist from "./components/Navbar/dropdownlist";
 
+import dropdownlist from "./components/Navbar/dropdownlist";
+import React from "react";
+import Carousel from "./components/Carousel/Carousel";
 export default function Home() {
   return (
     <div >
       <dropdownlist />
+      <Carousel />
         <h1>Home</h1>
     </div>
   );
