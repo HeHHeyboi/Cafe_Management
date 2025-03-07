da18f83ecd3064c555d05e11f94ca9001e24c8d1
HeHHeyboi <thanathatwave@gmail.com>
2025-03-07 15:46:40 +0700

    fix schema & build exe

--- backend/sql/schema/007_img.sql
+++ backend/sql/schema/007_img.sql
@@ -3,10 +3,7 @@ CREATE TABLE IF NOT EXISTS image(
 	menu_id INTEGER,
 	giveAway_id INTEGER,
 	gallery_name TEXT,
-	img_url TEXT NOT NULL,
-	FOREIGN KEY (menu_id) REFERENCES menu_id(menu_id),
-	FOREIGN KEY (giveAway_id) REFERENCES giveAway(id),
-	FOREIGN KEY (gallery_name) REFERENCES gallery(Gname)
+	img_url TEXT NOT NULL
 );
 -- +goose Down
 DROP TABLE image;
