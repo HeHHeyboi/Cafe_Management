e6bda6cc0a45229c91552753cdcd7967ae0d0553
HeHHeHboi <thanathatwave@gmail.com>
2025-03-11 20:59:24 +0700

    Fix when run npm run build

--- backend/main.go
+++ backend/main.go
@@ -103,7 +103,7 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001", "*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "X-Requested-With"},
 		AllowCredentials: true,
