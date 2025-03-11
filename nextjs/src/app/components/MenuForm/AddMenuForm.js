f0e471f798ca9c18b0cc5d0aefb49d1e67572037
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-03-12 04:56:47 +0700

    Add Button Gallery

--- nextjs/src/app/components/MenuForm/AddMenuForm.js
+++ nextjs/src/app/components/MenuForm/AddMenuForm.js
@@ -135,6 +135,7 @@ function AddMenuForm({ onMenuAdded }) {
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
+       
       <h2 className="text-xl font-semibold mb-4">เพิ่มเมนูใหม่</h2>
 
       {/* Success and Error Messages */}
