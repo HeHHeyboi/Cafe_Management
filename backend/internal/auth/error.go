61d28cd4edcd8d5c6f75eb5dc8f3fdc116d93c1f
HeHHeyboi <thanathatwave@gmail.com>
2025-09-17 15:10:37 +0700

    Create user_service.go

---
+++ backend/internal/auth/error.go
@@ -0,0 +1,13 @@
+package auth
+
+type HashError struct {
+	msg string
+}
+
+func (he *HashError) Error() string {
+	return he.msg
+}
+
+func NewHashError(msg string) *HashError {
+	return &HashError{msg}
+}
