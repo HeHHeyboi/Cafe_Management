472417ad26a3f7f28f031e2edbe4bb995903c685
HeHHeyboi <thanathatwave@gmail.com>
2025-09-17 15:10:37 +0700

    Create user_service.go

--- backend/internal/handler/user_handler.go
+++ backend/internal/handler/user_handler.go
@@ -1,1 +1,60 @@
 package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/HeHHeyboi/Cafe_Management/backend/internal/auth"
+	"github.com/HeHHeyboi/Cafe_Management/backend/internal/dto"
+	"github.com/HeHHeyboi/Cafe_Management/backend/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type UserHandler struct {
+	service service.UserService
+}
+
+func (uh *UserHandler) CreateUser(ctx *gin.Context) {
+	var userReq dto.UserRequest
+	var err error
+	if err = ctx.ShouldBind(&userReq); err != nil {
+		dto.BindingErrorMsg(err.(validator.ValidationErrors), ctx)
+		return
+	}
+	userRes, err := uh.service.CreateUser(ctx.Request.Context(), userReq)
+	if err != nil {
+		if errors.Is(err, &auth.HashError{}) {
+			errorResponse(401, "password Hash Error: %v", err, ctx)
+		} else {
+			msg := dto.CheckDataBaseError(err)
+			ctx.JSON(401, gin.H{"error": msg})
+			ctx.Error(err)
+		}
+		return
+	}
+	ctx.JSON(201, userRes)
+}
+
+func (uh *UserHandler) GetUser(ctx *gin.Context) {
+	var userReq dto.UserRequest
+	var err error
+	if err = ctx.ShouldBind(&userReq); err != nil {
+		dto.BindingErrorMsg(err.(validator.ValidationErrors), ctx)
+		return
+	}
+
+	response, err := uh.service.GetAllUser(ctx.Request.Context())
+	if err != nil {
+		errorResponse(401, "%v", err, ctx)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
+}
+
+func errorResponse(status int, fmtMsg string, err error, ctx *gin.Context) {
+	ctx.JSON(status, gin.H{"error": fmt.Sprintf(fmtMsg, err)})
+	ctx.Error(err)
+}
--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"github.com/HeHHeyboi/Cafe_Management/backend/internal/dto"
+	"github.com/google/uuid"
+)
 
 type User struct {
 	UserID   uuid.UUID
@@ -9,4 +12,23 @@ type User struct {
 	Email    string
 	Password string
 	Role     string
+}
+
+func (u *User) FromUserReq(userReq dto.UserRequest) {
+	u.UserID = userReq.UserID
+	u.Fname = userReq.Fname
+	u.Lname = userReq.Lname
+	u.Email = userReq.Email
+	u.Password = userReq.Password
+}
+
+func (u User) ToUserResponse() dto.UserResponse {
+	return dto.UserResponse{
+		UserID:   u.UserID,
+		Fname:    u.Fname,
+		Lname:    u.Lname,
+		Email:    u.Email,
+		Password: u.Password,
+		Role:     u.Role,
+	}
 }
