eff48723d13291c8be7c578ed1c7c29651a1ce49
HeHHeyboi <thanathatwave@gmail.com>
2025-09-17 23:36:52 +0700

    Now Can Connect to DataBase

---
+++ backend_java/src/main/resources/sql/schema/V2__gallery.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS gallery(
+	Gname TEXT PRIMARY KEY NOT NULL,
+	StartDate TEXT NOT NULL,
+	EndDate TEXT NOT NULL,
+	DESC TEXT,
+	user_id UUID,
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
