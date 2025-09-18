671246b24e30b11e5bb153ce2ab47865701684e4
HeHHeyboi <thanathatwave@gmail.com>
2025-09-19 01:04:39 +0700

    Test First API in Java SpringBoot Success

--- backend_java/src/main/java/com/CafeManagement/web/UserController.java
+++ backend_java/src/main/java/com/CafeManagement/web/UserController.java
@@ -1,7 +1,11 @@
 package com.CafeManagement.web;
 
+import com.CafeManagement.auth.*;
 import com.CafeManagement.dto.UserRequest;
+import com.CafeManagement.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	UserService service;
+
+	@Autowired
+	public UserController(UserService service) {
+		this.service = service;
+	}
 
 	@PostMapping
 	public ResponseEntity<String> CreateUser(@Valid @RequestBody UserRequest req) {
-		return ResponseEntity.ok(""" 
-			{ "msg":"Create user Succeed"}
-			""");
+		req.setPassword(Auth.HashPassword(req.getPassword()));
+		try {
+			service.CreateUser(req);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("""
+					{"Error" : "Can't Create User"} """);
+		}
+		return ResponseEntity.ok("""
+				{"msg" : "Create user Succeed"} """);
 	}
 }
