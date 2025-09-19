b9b63e7c87dc6b4902655988d7a2abdd0b2a599c
HeHHeyboi <thanathatwave@gmail.com>
2025-09-19 18:22:37 +0700

    Finish Basic UserApi

--- backend_java/src/main/java/com/CafeManagement/web/UserController.java
+++ backend_java/src/main/java/com/CafeManagement/web/UserController.java
@@ -1,16 +1,25 @@
 package com.CafeManagement.web;
 
-import com.CafeManagement.auth.*;
-import com.CafeManagement.dto.UserRequest;
-import com.CafeManagement.service.UserService;
-import jakarta.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CafeManagement.auth.Auth;
+import com.CafeManagement.dto.UserRequest;
+import com.CafeManagement.dto.UserResponse;
+import com.CafeManagement.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -34,4 +43,32 @@ public class UserController {
 		return ResponseEntity.ok("""
 				{"msg" : "Create user Succeed"} """);
 	}
+
+	@GetMapping
+	ResponseEntity<?> GetAllUser() {
+		List<UserResponse> response = new ArrayList<UserResponse>();
+		try {
+			response = service.GetAllUser();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("""
+					{"Error" : "%s"}""".formatted(e.toString()));
+		}
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/{id}")
+	@ResponseBody()
+	ResponseEntity<?> GetUserById(@PathVariable String id) {
+		UserResponse response = new UserResponse();
+		try {
+			response = service.GetUserById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("""
+					{"Error" : "%s"}""".formatted(e.toString()));
+		}
+		return ResponseEntity.ok(response);
+	}
+
 }
