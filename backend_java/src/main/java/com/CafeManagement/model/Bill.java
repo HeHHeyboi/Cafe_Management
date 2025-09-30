32e7574c23e888fc58f83f21a2ce5d26201db4a8
HeHHeyboi <thanathatwave@gmail.com>
2025-09-30 21:49:53 +0700

    Test MenuRequests

--- backend_java/src/main/java/com/CafeManagement/model/Bill.java
+++ backend_java/src/main/java/com/CafeManagement/model/Bill.java
@@ -4,44 +4,44 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Bill {
-    UUID bill_id;
-    LocalDateTime pay_date; // วันที่ + เวลา
-    double total;
+	UUID bill_id;
+	LocalDateTime pay_date; // วันที่ + เวลา
+	double total;
 
-    public Bill() {
+	public Bill() {
 
-    }
+	}
 
-    public Bill(UUID bill_id, LocalDateTime pay_date, double total) {
-        this.bill_id = bill_id;
-        this.pay_date = pay_date;
-        this.total = total;
-    }
+	public Bill(UUID bill_id, LocalDateTime pay_date, double total) {
+		this.bill_id = bill_id;
+		this.pay_date = pay_date;
+		this.total = total;
+	}
 
-    // getter
-    public UUID getBill_id() {
-        return bill_id;
-    }
+	// getter
+	public UUID getBill_id() {
+		return bill_id;
+	}
 
-    public LocalDateTime getPay_date() {
-        return pay_date;
-    }
+	public LocalDateTime getPay_date() {
+		return pay_date;
+	}
 
-    public double getTotal() {
-        return total;
-    }
+	public double getTotal() {
+		return total;
+	}
 
-    // setter
-    public void setBill_id(UUID bill_id) {
-        this.bill_id = bill_id;
-    }
+	// setter
+	public void setBill_id(UUID bill_id) {
+		this.bill_id = bill_id;
+	}
 
-    public void setPay_date(LocalDateTime pay_date;) {
-        this.pay_date = pay_date;
-    }
+	public void setPay_date(LocalDateTime pay_date) {
+		this.pay_date = pay_date;
+	}
 
-    public void setTotal(double total) {
-        this.total = total;
-    }
+	public void setTotal(double total) {
+		this.total = total;
+	}
 
 }
