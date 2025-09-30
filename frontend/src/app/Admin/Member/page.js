d2537284fc24684efd0a3a2d674ea696f7083c71
Simon <162421836+Simonlee789@users.noreply.github.com>
2025-09-30 15:56:48 +0700

    Add Member page and sidebar link in admin panel
    Created a new Member page under the admin section and added a corresponding link to the AdminSidebar navigation. This provides access to the Member page from the admin sidebar.

---
+++ frontend/src/app/Admin/Member/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MemberPage() {
+  return (
+    <div >
+      <h1 className=' flex text-4xl font-bold align-center justify-center content-center'>
+         ยูฟ่าอย่าลืมมาทำนะ</h1>
+    </div>
+  )
+}
