c74e903360d3ae1b77eec84b56441c7b788f81d8
HeHHeyboi <thanathatwave@gmail.com>
2025-10-01 18:18:00 +0700

    Try Multipart File and update doc

--- backend_java/src/main/java/com/CafeManagement/web/MenuController.java
+++ backend_java/src/main/java/com/CafeManagement/web/MenuController.java
@@ -2,15 +2,19 @@ package com.CafeManagement.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.json.JacksonJsonDecoder;
+import org.springframework.http.codec.json.JacksonJsonEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.CafeManagement.dto.MenuRequest;
 import com.CafeManagement.service.MenuService;
 
 import jakarta.validation.Valid;
+import tools.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/menu")
@@ -22,8 +26,13 @@ public class MenuController {
 		this.service = service;
 	}
 
-	@PostMapping
-	public ResponseEntity<String> addCustomer(@Valid @RequestBody MenuRequest req) {
+	@PostMapping(consumes = { "multipart/form-data" })
+	public ResponseEntity<String> addCustomer(@RequestParam("img") MultipartFile file,
+			@Valid @RequestParam("data") String data) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		MenuRequest req = mapper.readValue(data, MenuRequest.class);
+
 		try {
 			service.CreateMenu(req, null);
 		} catch (Exception e) {
@@ -35,6 +44,8 @@ public class MenuController {
 					""");
 		}
 
-		return ResponseEntity.ok("Create Menu Success");
+		return ResponseEntity.ok("""
+				{"msg":"Create Menu Success","img":"%s"}
+				""".formatted(file.getName()));
 	}
 }
