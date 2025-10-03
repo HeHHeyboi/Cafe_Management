b1bc10c622425b643566bb0952ce5f2cf4337601
HeHHeyboi <thanathatwave@gmail.com>
2025-10-03 13:52:18 +0700

    Finish BillController ยังไม่ได้ Test

--- backend_java/src/main/java/com/CafeManagement/web/BillController.java
+++ backend_java/src/main/java/com/CafeManagement/web/BillController.java
@@ -1,14 +1,25 @@
 package com.CafeManagement.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.CafeManagement.dto.BillResponse;
+import com.CafeManagement.dto.UpdateBillRequest;
+import com.CafeManagement.exception.BillNotFoundException;
 import com.CafeManagement.service.BillService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/bill")
@@ -35,63 +46,72 @@ public class BillController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
-	/*
-	 * NOTE: Get all Bills
-	 * 
-	 * @GetMapping
-	 * public ResponseEntity<?> getAllBills() {
-	 * List<BillResponse> responses = new ArrayList<>();
-	 * try {
-	 * responses = service.GetAllBills();
-	 * } catch (Exception e) {
-	 * e.printStackTrace();
-	 * return ResponseEntity.internalServerError().body("""
-	 * {"error":"%s"}
-	 * """.formatted(e.getMessage()));
-	 * }
-	 * return ResponseEntity.ok(responses);
-	 * }
-	 */
+	@GetMapping
+	public ResponseEntity<?> getAllBills() {
+		List<BillResponse> responses = new ArrayList<>();
+		try {
+			responses = service.GetAllBills();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("""
+					{"error":"%s"}
+					""".formatted(e.getMessage()));
+		}
+		return ResponseEntity.ok(responses);
+	}
 
-	/*
-	 * NOTE: Update Bill by Id
-	 * 
-	 * @PutMapping("/{id}")
-	 * public ResponseEntity<String> updateBillById(
-	 * 
-	 * @Valid @RequestBody BillRequest req, @PathVariable int id) {
-	 * try {
-	 * service.UpdateBillById(req, id);
-	 * } catch (BillNotFoundException notfound) {
-	 * return ResponseEntity.status(HttpStatus.NOT_FOUND).body("""
-	 * {"error":"%s"}
-	 * """.formatted(notfound.getMessage()));
-	 * } catch (Exception e) {
-	 * e.printStackTrace();
-	 * return ResponseEntity.internalServerError().body("""
-	 * {"error":"%s"}
-	 * """.formatted(e.getMessage()));
-	 * }
-	 * return ResponseEntity.ok("""
-	 * {"msg":"Bill updated successfully"}
-	 * """);
-	 * }
-	 */
+	// Get Bill By ID
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getBillById(@PathVariable String id) {
+		BillResponse response;
+		try {
+			response = service.GetBill(id);
+		} catch (BillNotFoundException notfound) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("""
+					{"error":"%s"}
+					""".formatted(notfound.getMessage()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("""
+					{"error":"%s"}
+					""".formatted(e.getMessage()));
+		}
+		return ResponseEntity.ok(response);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateBillById(@Valid @RequestBody UpdateBillRequest req, @PathVariable String id) {
+		try {
+			service.UpdateBillById(req, id);
+		} catch (BillNotFoundException notfound) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("""
+					{"error":"%s"}
+					""".formatted(notfound.getMessage()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("""
+					{"error":"%s"}
+					""".formatted(e.getMessage()));
+		}
+		return ResponseEntity.ok("""
+				{"msg":"Bill updated successfully"}
+				""");
+	}
 
 	// NOTE: Delete bill by id
-	// @DeleteMapping("/{id}")
-	// public ResponseEntity<String> deleteBillById(@PathVariable int id) {
-	// try {
-	// service.DeleteBillById(id);
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// return ResponseEntity.internalServerError().body("""
-	// {"error":"%s"}
-	// """.formatted(e.getMessage()));
-	// }
-	// return ResponseEntity.ok("""
-	// {"msg":"Bill deleted successfully"}
-	// """);
-	// }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteBillById(@PathVariable String id) {
+		try {
+			service.DeleteBillById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("""
+					{"error":"%s"}
+					""".formatted(e.getMessage()));
+		}
+		return ResponseEntity.ok("""
+				{"msg":"Bill deleted successfully"}
+				""");
+	}
 
 }
